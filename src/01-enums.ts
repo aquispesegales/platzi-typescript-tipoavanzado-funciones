cccb62e4f44fb0999d647cb301f3068a109693dd
aquispesegales <alvaroquispesegales@gmail.com>
2024-01-12 14:51:53 -0400

    VIDEO TUPLAS TERMINADO

--- src/01-enums.ts
+++ src/01-enums.ts
@@ -3,3 +3,13 @@ enum ROLES {
     SELLER = "seller",
     CUSTOMER = "customer",
 }
+
+type User = {
+    username : string,
+    role: ROLES
+}
+
+const nicoUser : User= {
+    username: 'nicobytes',
+    role:ROLES.ADMIN
+};
---
+++ src/02-tuples.ts
@@ -0,0 +1,9 @@
+const prices:(number|string)[] = [1,3,2,2,2,'as'];
+prices.push(1);
+prices.push('1');
+
+let user:[string,number,boolean];
+user = ['nico',12,true];
+const [username,age] = user;
+console.log(username);
+console.log(age);
